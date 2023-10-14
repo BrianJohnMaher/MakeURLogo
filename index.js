3f7f50ebe663d223fa4e5f26b18e7910b6848558
BRIAN MAHER <bmaher22@mac.comclear>
2023-10-14 14:56:13 -0400

    work in progess

---
+++ index.js
@@ -0,0 +1,13 @@
+// needs to have the inquire and fs package
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Circle }=  require('./lib/shape')
+
+
+function init(){
+
+    // it need to ask the user 4 questions: 1. what shape? (circle, triangle, square) 2.text color. 3. text to add to the shape. 4. the shape color
+    
+    // once we have collected the response we need to create the file that will have the svg created in it. 
+}
+init()
