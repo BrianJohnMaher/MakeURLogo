609f492a27dbeffd2db5a2e04feaab72ee255542
BRIAN MAHER <bmaher22@mac.comclear>
2023-10-17 16:31:12 -0400

    finished with the coding just have to record and submit

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
 // needs to have the inquire and fs package
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Circle }=  require('./lib/shape')
+const { Circle, Square, Triangle } = require('./lib/shape')
 
 
-function init(){
+function init() {
 
     // it need to ask the user 4 questions: 1. what shape? (circle, triangle, square) 2.text color. 3. text to add to the shape. 4. the shape color
     inquirer.prompt([
         {
-            type:'list', 
+            type: 'list',
             name: 'shape',
-            message: 'What Shape do you choose?', 
+            message: 'What Shape do you choose?',
             choices: ['circle', 'triangle', 'square']
-        }, 
+        },
         {
-            type:'input', 
+            type: 'input',
             name: 'shapeColor',
-            message: 'What Shape color do you choose?', 
-          
-        }, 
+            message: 'What Shape color do you choose?',
+
+        },
         {
-            type:'input', 
+            type: 'input',
             name: 'text',
-            message: 'What text do you choose?', 
-          
-        }, 
+            message: 'What text do you choose?',
+
+        },
         {
-            type:'input', 
+            type: 'input',
             name: 'textColor',
-            message: 'What text color do you choose?', 
-            
-        }, 
-    ]).then((answer)=> {
-        
+            message: 'What text color do you choose?',
+
+        },
+    ]).then((answer) => {
+
         let choice;
         switch (answer.shape) {
             case 'circle':
-     
+
                 choice = new Circle(answer.shapeColor, answer.text, answer.textColor)
                 break;
-        
-            default:
-                console.log('no other choice');
+            case 'triangle':
+
+                choice = new Triangle(answer.shapeColor, answer.text, answer.textColor)
                 break;
+
+            default:
+                choice = new Square(answer.shapeColor, answer.text, answer.textColor)
+                break;
+
         }
 
         fs.writeFile('./dist/logo.svg', choice.render(), (err, res) => {
